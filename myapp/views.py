0c49ec12436198657206f09067d51a5646b5b417
Nawasan Wisitsingkhon <arikato110011@gmail.com>
2023-07-02 12:19:47 +0700

    POST and PUT database

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from myapp.models import Person
 
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+    all_person = Person.objects.all()
+    return render(request, "index.html", {"users": all_person})
 
 def about(request):
     name = "Nawasan Wisitsingkhon"
@@ -12,4 +15,27 @@ def about(request):
     return render(request, 'about.html', {"name":name,"age": age})
 
 def form(request):
-    return render(request, 'form.html')
+    if request.method == "POST":
+        name = request.POST['name']
+        age = request.POST['age']
+        person = Person.objects.create(
+            name=name,
+            age=age
+        )
+        person.save()
+        messages.success(request, "บันทึกข้อมูลเรียบร้อย")
+        return redirect('/')
+    else:
+        return render(request, 'form.html')
+
+def edit(request, person_id):
+    if request.method == "POST":
+        person = Person.objects.get(id=person_id)
+        person.name = request.POST["name"]
+        person.age = request.POST["age"]
+        person.save()
+        messages.success(request, "อัพเดทข้อมูลเรียบร้อย")
+        return redirect('/')
+
+    person = Person.objects.get(id=person_id)
+    return render(request, 'edit.html', {"person": person})
--- script.py
+++ script.py
@@ -1,7 +1,11 @@
 import sys
 import os
+import platform
 
 defaltManage = "python3 manage.py"
+if("Windows" == platform.system()):
+   defaltManage = "python manage.py"
+
 scripts = {
     "dev": f"{defaltManage} runserver",
     "db-make": f"{defaltManage} makemigrations",
@@ -9,8 +13,9 @@ scripts = {
     "admin": f"{defaltManage} createsuperuser"
 }
 
-if sys.argv[1] in scripts.keys():
-   print("~run: ", scripts[sys.argv[1]], '\n')
-   os.system(scripts[sys.argv[1]]) 
+if len(sys.argv) > 1:
+   if sys.argv[1] in scripts.keys():
+      print("~run: ", scripts[sys.argv[1]], '\n')
+      os.system(scripts[sys.argv[1]]) 
 else:
    print("Not found script")
